01354a4fe740966cef54e89844298cae66156220
bayufazri <bayufazri17@gmail.com>
2021-08-31 15:06:49 +0700

    tampilan dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,16 @@
+<div id="sidebar-nav" class="sidebar">
+    <div class="sidebar-scroll">
+        <nav>
+            <ul class="nav">
+                <li><a href="#"><i class="fa fa-home fa-lg"></i><span style="margin-left:10px;">Dashboard</span></a></li>
+                <li><a href="#" class=""><i class="fa fa-users fa-lg"></i><span style="margin-left:8px;">User</span></a></li>
+                <li><a href="#" class=""><i class="fa fa-mortar-board fa-lg"></i><span style="margin-left:5px;">Siswa</span></a></li>
+                <li><a href="#" class=""><i class="fa fa-user-secret fa-lg"></i><span style="margin-left:12px;">Pembimbing</span></a></li>
+				<li><a href="#" class=""><i class="fa fa-address-book fa-lg"></i><span style="margin-left:10px;">Rayon</span></a></li>
+				<li><a href="#" class=""><i class="fa fa-address-card fa-lg"></i><span style="margin-left:8px;">Jurusan</span></a></li>
+				<li><a href="#" class=""><i class="fa fa-credit-card fa-lg"></i><span style="margin-left:9px;">Angkatan</span></a></li>
+				<li><a href="#" class=""><i class="fa fa-money fa-lg"></i><span style="margin-left:8px;">Jenis Pembayaran</span></a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
